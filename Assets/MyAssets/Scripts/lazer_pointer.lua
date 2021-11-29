0587203b2a35af5a34a063a01ee9f70ef2076c94
koduki <pascalm3+git@gmail.com>
2021-11-28 16:14:13 -0800

    pointer paring

--- Assets/MyAssets/Scripts/lazer_pointer.lua
+++ Assets/MyAssets/Scripts/lazer_pointer.lua
@@ -1,63 +1,65 @@
 print("lazer-pointer: ver01")
 
 local Lazer = {
-     obj = vci.assets.GetSubItem("Lazer"),
-     length = 5,
-     lightSource = 0.52 + 2 * 5
+    obj = vci.assets.GetSubItem("Lazer"),
+    length = 5,
+    lightSource = 0.52 + 2 * 5
 }
 local Stick = vci.assets.GetSubItem("Stick")
 local DummyLazer = vci.assets.GetSubItem("DummyLazer")
 local BoardStatus = "" --'left'|'right'
 
+local Msgid = ""
+
 function SetLazerLength(len)
-     Lazer.length = len
-     Lazer.lightSource = 0.52 + 2 * len
-     print(string.format("Lazer.length: %d, Lazer.lightSource: %d", Lazer.length, Lazer.lightSource))
+    Lazer.length = len
+    Lazer.lightSource = 0.52 + 2 * len
+    print(string.format("Lazer.length: %d, Lazer.lightSource: %d", Lazer.length, Lazer.lightSource))
 end
 
 function TurnOnLazer()
-     local scale = Vector3.__new(0.005, 0.005, Lazer.length)
-     local pos = Vector3.__new(0, 0, Lazer.lightSource)
-     DummyLazer.SetLocalPosition(pos)
-     DummyLazer.SetLocalScale(scale)
+    local scale = Vector3.__new(0.005, 0.005, Lazer.length)
+    local pos = Vector3.__new(0, 0, Lazer.lightSource)
+    DummyLazer.SetLocalPosition(pos)
+    DummyLazer.SetLocalScale(scale)
 end
 
 function TurnOffLazer()
-     local scale = Vector3.__new(0.005, 0.005, 0)
-     local pos = Vector3.__new(Stick.getLocalPosition().x, Stick.getLocalPosition().y, 0)
-     DummyLazer.SetLocalPosition(pos)
-     DummyLazer.SetLocalScale(scale)
+    local scale = Vector3.__new(0.005, 0.005, 0)
+    local pos = Vector3.__new(Stick.getLocalPosition().x, Stick.getLocalPosition().y, 0)
+    DummyLazer.SetLocalPosition(pos)
+    DummyLazer.SetLocalScale(scale)
 end
 
 function onGrab(use)
-     TurnOnLazer()
+    TurnOnLazer()
 end
 
 function onUngrab(use)
-     TurnOffLazer()
+    TurnOffLazer()
 end
 
 function updateAll()
-     Lazer.obj.SetPosition(DummyLazer.GetPosition())
-     Lazer.obj.SetRotation(DummyLazer.GetRotation())
-     Lazer.obj.SetLocalScale(DummyLazer.GetLocalScale())
+    Lazer.obj.SetPosition(DummyLazer.GetPosition())
+    Lazer.obj.SetRotation(DummyLazer.GetRotation())
+    Lazer.obj.SetLocalScale(DummyLazer.GetLocalScale())
 end
 
 function onTriggerEnter(item, hit)
-     if (item == "Lazer") then
-          if (hit == "LeftScreen") then
-               BoardStatus = "left"
-               print(string.format("onTriggerEnter: item=%s, item=%s", item, hit))
-          elseif (hit == "RightScreen") then
-               BoardStatus = "right"
-               print(string.format("onTriggerEnter: item=%s, item=%s", item, hit))
-          end
-     end
-end
-
-function onTriggerExit(item, hit)
+    if (item == "Lazer") then
+        if (hit == "LeftScreen") then
+            BoardStatus = "left"
+            print(string.format("onTriggerEnter: item=%s, item=%s", item, hit))
+        elseif (hit == "RightScreen") then
+            BoardStatus = "right"
+            print(string.format("onTriggerEnter: item=%s, item=%s", item, hit))
+        elseif (hit == "Board") then
+            Msgid = os.date("%Y%m%d%H%M%S")
+            print(string.format("Slide Message ID: %s", Msgid))
+        end
+    end
 end
 
 function onUse()
-     vci.message.EmitWithId("sendFromLazerPointer121", { event = BoardStatus })
-end 
+    vci.message.EmitWithId("sendFromLazerPointer", {event = BoardStatus, msgid = Msgid})
+end
--- Assets/MyAssets/Scripts/slide.lua
+++ Assets/MyAssets/Scripts/slide.lua
@@ -1,14 +1,16 @@
-print("slideboard: ver05")
+print("slideboard: ver01")
 
-local MAX_SLIDE_PAGE = 8
-local MAX_SLIDE_X_INDEX = 4
-local MAX_SLIDE_Y_INDEX = 2
+local MAX_SLIDE_PAGE = 22
+local MAX_SLIDE_X_INDEX = 10
+local MAX_SLIDE_Y_INDEX = 3
 
 local UseCount = 0
-local DummyRightScreen = vci.assets.GetTransform("DummyRightScreen")
-local DummyLeftScreen  = vci.assets.GetTransform("DummyLeftScreen")
-local RightScreen      = vci.assets.GetTransform("RightScreen")
-local LeftScreen       = vci.assets.GetTransform("LeftScreen")
+local DummyRightScreen = vci.assets.GetSubItem("DummyRightScreen")
+local DummyLeftScreen = vci.assets.GetSubItem("DummyLeftScreen")
+local RightScreen = vci.assets.GetSubItem("RightScreen")
+local LeftScreen = vci.assets.GetSubItem("LeftScreen")
+
+local Msgid = ""
 
 function NextSlide(boardStatus)
     if boardStatus == "right" then
@@ -26,7 +28,7 @@ function NextSlide(boardStatus)
     end
 
     local xidx = UseCount % MAX_SLIDE_X_INDEX
-    local yidx = math.ceil((UseCount + 1 ) / MAX_SLIDE_X_INDEX) % MAX_SLIDE_Y_INDEX
+    local yidx = math.ceil((UseCount + 1) / MAX_SLIDE_X_INDEX) % MAX_SLIDE_Y_INDEX
     local width = (1.0 / MAX_SLIDE_X_INDEX)
     local height = (1.0 / MAX_SLIDE_Y_INDEX)
     local offset = Vector2.zero
@@ -34,67 +36,46 @@ function NextSlide(boardStatus)
     offset.x = width * xidx
 
     local page = UseCount % (MAX_SLIDE_X_INDEX * MAX_SLIDE_Y_INDEX) + 1
-    print(String.format("onUse: %d, index-x: %d, index-y: %d, page: %d, offset.x: %f, offset.y: %f", UseCount, xidx, yidx, page, offset.x, offset.y))
+    print(
+        string.format(
+            "onUse: %d, index-x: %d, index-y: %d, page: %d, offset.x: %f, offset.y: %f",
+            UseCount,
+            xidx,
+            yidx,
+            page,
+            offset.x,
+            offset.y
+        )
+    )
 
     vci.assets._ALL_SetMaterialTextureOffsetFromName("Slide", offset)
 end
 
 function OnLazerPointerMessage(sender, name, message)
-    print(message.event)
-    NextSlide(message.event)
+    print(string.format("sender messageID:%s, own messageID:%s, event:%s", message.msgid, Msgid, message.event))
+    if (message.msgid == Msgid) then
+        NextSlide(message.event)
+    end
 end
-vci.message.On("sendFromLazerPointer121", OnLazerPointerMessage)
+vci.message.On("sendFromLazerPointer", OnLazerPointerMessage)
 
 function onUse(use)
-    -- NextSlide("right")
-    print("onUse")
-
-    local cam = vci.studio.GetHandiCamera()
-    cam.SetPosition(Vector3.__new(0, 1, 0))
+    NextSlide("right")
 end
 
 function updateAll()
-    local cam = vci.studio.GetHandiCamera()
-    local camPos = cam.GetPosition()
-    local scrnPos = LeftScreen.GetPosition()
-    print(string.format("camPos : %f, %f, %f", camPos.x, camPos.y, camPos.z))
-    print(string.format("scrnPos: %f, %f, %f", scrnPos.x, scrnPos.y, scrnPos.z))
-
-    local dist = math.sqrt((camPos.x - scrnPos.x)^2 + (camPos.y - scrnPos.y)^2)
-    print(string.format("dist: %f", dist))
-
-
-    local r = 0
-    local angle = DummyLeftScreen.GetRotation().eulerAngles
-    print(angle)
-    -- print(String.format("angle: %f, %f, %f", angle.x, angle.y, angle.z))
-    -- local x = r * math.sin(angle.z) * math.cos(angle.x)
-    -- local y = r * math.sin(angle.z) * math.sin(angle.x)
-    -- local z = r * math.cos(angle.z)
-
-    local t = angle.y * math.pi/180
-    local x = r * math.sin(t) + scrnPos.x
-    local y = r * math.sin(angle.x) * math.sin(angle.y)
-    local z = r * math.cos(t) + scrnPos.z
-    local pos = Vector3.__new(x, 1, z)
-    print(string.format("camPos2: %f, %f, %f", pos.x, pos.y, pos.z))
-    -- cam.SetPosition(pos)
-
-    -- cam.SetRotation(LeftScreen.GetRotation())
-    local rotate = Quaternion.AngleAxis(DummyLeftScreen.GetRotation().eulerAngles.y , Vector3.up)
-    cam.SetRotation(rotate)
-
-    -- print("pos2")
-    -- print(RightScreen.GetPosition())
-
     RightScreen.SetPosition(DummyRightScreen.GetPosition())
     RightScreen.SetRotation(DummyRightScreen.GetRotation())
     LeftScreen.SetPosition(DummyLeftScreen.GetPosition())
     LeftScreen.SetRotation(DummyLeftScreen.GetRotation())
-    -- math.sin
+
     local scale = vci.assets.GetSubItem("Board").GetLocalScale()
     scale.z = 6.123234e-17
     scale.x = scale.x - 0.1
     RightScreen.SetLocalScale(scale)
     LeftScreen.SetLocalScale(scale)
+end
+
+function onTriggerEnter(item, hit)
+    Msgid = os.date("%Y%m%d%H%M%S")
 end
